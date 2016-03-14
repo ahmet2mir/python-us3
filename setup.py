20a603873f998e3712c93d20c5036f94399bb263
Ahmet Demir <me@ahmet2mir.eu>
2016-03-14 17:51:28 +0100

    firt commit and not the last

---
+++ setup.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+#!/usr/bin/env python
+"""
+Upload to S3
+
+Licence
+```````
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+__author__ = "Ahmet Demir <me@ahmet2mir.eu>"
+
+from setuptools import setup, find_packages
+
+version = '0.0.1'
+
+setup(
+    name='us3',
+    version=version,
+    long_description=open('README.md').read(),
+    author='Ahmet Demir',
+    author_email='me@ahmet2mir.eu',
+    description='Upload to S3',
+    license='License :: OSI Approved :: Apache Software License',
+    keywords=['s3', 'command line', 'cli'],
+    packages=find_packages(),
+    package_data = {'': ['README.md']},
+    install_requires=open('requirements.txt').read().splitlines(),
+    entry_points={
+        'console_scripts': [
+            'us3=us3.us3:main'
+        ],
+    }
+)
